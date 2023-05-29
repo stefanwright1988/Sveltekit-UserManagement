ba5ac52e7f4066ea795bd65f54d7fc8115389ba8
Stefan Wright <stefanwright1988@gmail.com>
2023-05-29 21:34:36 +0100

    Updated env var

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,11 +1,12 @@
-import { PRIVATE_SUPABASE_ANON_KEY, PRIVATE_SUPABASE_URL, VERCEL_COMMIT_REF } from '$env/static/private';
+import { PRIVATE_SUPABASE_ANON_KEY, PRIVATE_SUPABASE_URL, VERCEL_GIT_COMMIT_REF } from '$env/static/private';
 import type { LayoutServerLoad } from './$types'
+
 
 export const load: LayoutServerLoad = async ({ locals: { getSession } }) => {
     return {
         supabaseAnonKey: PRIVATE_SUPABASE_ANON_KEY,
         supabaseUrl: PRIVATE_SUPABASE_URL,
-        commitRef: VERCEL_COMMIT_REF,
+        commitRef: VERCEL_GIT_COMMIT_REF,
         session: await getSession(),
     }
 }
