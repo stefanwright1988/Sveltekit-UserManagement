a5e114ceddc62c260fd28425d691ebf7daa5e283
Stefan Wright <stefanwright1988@gmail.com>
2023-05-29 20:46:31 +0100

    Initial commit
    I meant to commit a long time ago!

---
+++ src/routes/+page.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+    import { Auth } from '@supabase/auth-ui-svelte';
+    import { ThemeSupa } from '@supabase/auth-ui-shared';
+	
+    export let data;
+</script>
+
+<svelte:head>
+    <title>User Management - Login</title>
+</svelte:head>
+
+<div class="row flex-center flex">
+    <div class="col-6 form-widget">
+        <Auth
+            supabaseClient={data.supabase}
+            view="magic_link"
+            redirectTo={`${data.url}/logging-in?redirect=/`}
+            showLinks={false}
+            appearance={{theme: ThemeSupa, style: {input: 'color: #fff'}}}
+            additionalData={{data:null}}
+            />
+    </div>
+</div>
